66aa94e456b6401bc5f4a44041d3f621ffbc9f6c
Thales César <thalesgiriboni@gmail.com>
2020-02-04 21:16:39 -0300

    Add rand dependency and expand game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -10,4 +13,6 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    println!("The secret number is: {}", secret_number);
 }
