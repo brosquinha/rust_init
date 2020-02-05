7e122cfa9e8b8c391e1f441564879369527b99ba
Thales César <thalesgiriboni@gmail.com>
2020-02-04 21:28:09 -0300

    Add loop to guessing game

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,25 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
+            
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a valid number!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a valid number!");
+        println!("You guessed: {}", guess);
+        // println!("The secret number is: {}", secret_number);
 
-    println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win! Congrats! 🎉"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! Congrats! 🎉");
+                break;
+            }
+        }
     }
 }
